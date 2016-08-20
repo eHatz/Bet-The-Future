c0f83b1622d6fafbbd16cc08120516913f2d5878
Nathaniel LaPier <Nat@Nathaniels-MacBook-Air.local>
2016-08-20 12:27:50 -0400

    Added a logic.js file, and added validation to User model

---
+++ logic.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
+var sequelize = require('sequelize');
+var routes = require('./controllers/bet_controllers.js');
+
+var app = express();
+
+//Check new user info
+module.exports = {
+	//We should be able to use the sequelize-isunique-validator npm package instead of this method; keeping this method for now, JIC
+	userCheck: function(newName, newEmail, table){
+		//Select all current users from DB
+		table.findAll().then(function(data){
+			//Loop through the resulting array of users
+			var email = true;
+			var name = true;
+			data.forEach(function(userObj){
+				//If the new user's User Name or Email matches that of a currrent user, return false
+				if (userObj.userName === newName){
+					name = false;
+				};
+				if (userObj.email === newEmail){
+					email = false
+				}
+			})
+			var output = {
+				email: email, 
+				name: name};
+			return output;
+		})
+	},
+
+
+
+}
+
