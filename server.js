db9cacf6567b166a83af73babd562bc69c6ea5b5
Jennine Punzone <jennine@Jennine-Punzones-MacBook-Pro.local>
2016-08-26 13:53:32 -0400

    betbox

--- server.js
+++ server.js
@@ -15,19 +15,18 @@ var SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 var routes = require('./controllers/bet_controllers.js');
 var models = require('./models');
-<<<<<<< HEAD
+
 var User = models.User; //correct?
-=======
-var User = models.Users; //correct?
-User.findOne().then(function(userArray){
-  console.log(userArray);
+// var User = models.Users; //correct?
+// User.findOne().then(function(userArray){
+//   console.log(userArray);
  
-});
+// });
 
 // models.Bet.findAll().then(function(betArray){
 //   console.log(betArray);
 // });
->>>>>>> ec45ba2cc3f09676a757996014f5b9ca1326fd4b
+
 
 var app = express();
 var router = express.Router();
