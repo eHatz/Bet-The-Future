3a8c5dcda7a4da0a17ca19cb19a2d450cefa8fc9
Nathaniel LaPier <Nat@Nathaniels-MacBook-Air.local>
2016-08-30 15:07:58 -0400

    changed config.json and server.js for deployment

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var exphbs = require('express-handlebars');
 var sequelize = require('sequelize');
 var bcrypt = require('bcrypt-nodejs');
 var cookieParser = require('cookie-parser');
+var mysql = require('mysql');
 
 //Passport dependencies
 var passport = require('passport');
@@ -13,32 +14,16 @@ var LocalStrategy = require('passport-local').Strategy;
 var session = require('express-session');
 var SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+//Express setup
+var app = express();
+var router = express.Router();
+
+//Local dependencies
 var routes = require('./controllers/bet_controllers.js');
 var models = require('./models');
 
-
-var User = models.User; //correct?
-
-var User = models.Users; //correct?
-
-//================FIND ONE==============
-// User.findOne().then(function(userArray){
-//   console.log(userArray);
- 
-// });
-//================FIND ALL==================
-// models.Bet.findAll().then(function(betArray){
-//   console.log(betArray);
-// });
-//===========================================
-
-
-
-var User = models.User; 
-
-
-var app = express();
-var router = express.Router();
+//Sync 
+models.sequelize.sync()
 
 //Middleware
 app.use(express.static(__dirname + '/public'));
@@ -70,7 +55,21 @@ app.listen(PORT, function() {
     console.log("app is listening on port ", PORT);
 });
 
+//Heroku Deployment
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
+
 /////////// PASSPORT \\\\\\\\\\\\
+var User = models.User; 
 
 passport.serializeUser(function(user, done) {
   done(null, user);
